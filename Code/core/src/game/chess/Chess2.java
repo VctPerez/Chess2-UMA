c8d0603e5eec61d3496441b2d68ea34caa61903f
Victor Perez <vparmenta@uma.es>
2023-03-20 13:11:57 +0100

    Update Chess2.java

--- Code/core/src/game/chess/Chess2.java
+++ Code/core/src/game/chess/Chess2.java
@@ -1,32 +1,29 @@
 package game.chess;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import utils.Render;
 
 public class Chess2 extends ApplicationAdapter {
-	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-		System.out.println("Adriduty");
+		Render.Batch = new SpriteBatch();
+		Render.camara = new OrthographicCamera();
+		Render.app = this;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+
 	}
 }
