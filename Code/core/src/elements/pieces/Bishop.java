6ab31176622f3975a968d1b6e8e5eb86d1205c35
JoseBueno30 <jbueno3830@uma.es>
2023-03-22 12:07:49 +0100

    alfil

--- Code/core/src/elements/pieces/Bishop.java
+++ Code/core/src/elements/pieces/Bishop.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.math.Vector2;
 
+import elements.Board;
 import elements.Piece;
+import game.chess.GameScreen;
 import utils.Image;
 import utils.Resources;
 
@@ -20,8 +22,13 @@ public class Bishop extends Piece{
 	public void draw(Batch batch, float parentAlpha) {
 		super.draw(batch, parentAlpha);
 	}
+	
+	public Boolean checkBoard(Board board, float x, float y) {
+		return true;
+	}
+	
 	/**
-	 * A�ade a movements todos los movimientos posibles del Alfil, en todas las direcciones, su maxima cantidad de movimientos
+	 * Anyade a movements todos los movimientos posibles del Alfil, en todas las direcciones, su maxima cantidad de movimientos
 	 * @param x
 	 * @param y
 	 * @return
@@ -29,12 +36,26 @@ public class Bishop extends Piece{
 	@Override
 	public ArrayList<Vector2> getMovement(float x, float y) {
 		ArrayList<Vector2> movements = new ArrayList<>();
-		for(int i=1;i<8;i++) {
+		Boolean validDirection = true;
+		Vector2 mov;
+		int i = 1;
+		for(int j=1;i<8;i++) {
 			movements.add(new Vector2(x+i,y+i));
 			movements.add(new Vector2(x-i,y-i));
 			movements.add(new Vector2(x+i,y-i));
 			movements.add(new Vector2(x-i,y+i));
 		}
+		
+		while(validDirection && i<8) {
+			mov = new Vector2(x + i, y + i);
+			if(checkBoard(GameScreen.board, mov.x, mov.y)) {
+				movements.add(mov);
+			}
+			
+			i++;
+		}
+		
+		
 		return movements;
 	}
 	
