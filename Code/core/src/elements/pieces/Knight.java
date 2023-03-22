99a54d22bc45423c7cd3c4109903bff765cef291
JoseBueno30 <jbueno3830@uma.es>
2023-03-22 17:07:47 +0100

    Caballo bomba defusado

--- Code/core/src/elements/pieces/Knight.java
+++ Code/core/src/elements/pieces/Knight.java
@@ -34,15 +34,22 @@ public class Knight extends Piece{
 	@Override
 	public ArrayList<Vector2> getMovement(float x, float y) {
 		ArrayList<Vector2> movements = new ArrayList<>();
-		movements.add(new Vector2(x+2,y+1));
-		movements.add(new Vector2(x+1,y+2));
-		movements.add(new Vector2(x+2,y-1));
-		movements.add(new Vector2(x+1,y-2));
-		movements.add(new Vector2(x-1,y-2));
-		movements.add(new Vector2(x-2,y+1));
-		movements.add(new Vector2(x-2,y-1));
-		movements.add(new Vector2(x-1,y+2));
-		return getValidMovements(movements,GameScreen.board);
+		Board board = GameScreen.board;
+		addMovement(x+2,y+1, board, movements);
+		addMovement(x+1,y+2, board, movements);
+		addMovement(x+2,y-1, board, movements);
+		addMovement(x+1,y-2, board, movements);
+		addMovement(x-1,y-2, board, movements);
+		addMovement(x-2,y+1, board, movements);
+		addMovement(x-2,y-1, board, movements);
+		addMovement(x-1,y+2, board, movements);
+		return movements;
+	}
+	
+	public void addMovement(float x, float y, Board board, ArrayList<Vector2> movements) {
+		if(board.getTile(x, y)!=null && !sameColor(board.getTile(x, y).getPiece())) {
+			movements.add(new Vector2(x, y));
+		}
 	}
 	
 	private Boolean sameColor(Piece piece) {
@@ -51,16 +58,6 @@ public class Knight extends Piece{
 			same=color==piece.color();
 		}
 		return same;
-	}
-	
-	public ArrayList<Vector2> getValidMovements(ArrayList<Vector2> movements,Board board){
-		for(int i=0;i<movements.size();i++) {
-			if(board.getTile(movements.get(i).x, movements.get(i).y)==null 
-				|| sameColor(board.getTile(movements.get(i).x, movements.get(i).y).getPiece())) {
-				movements.remove(i);
-			}
-		}
-		return movements;
 	}
 	
 	
