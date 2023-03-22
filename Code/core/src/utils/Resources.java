b0821792b83c71b6dc50d5c9201cab3f3f0101a7
DavidBueno <davidbuenocarmona@uma.es>
2023-03-22 12:09:14 +0100

    Update Resources.java

--- Code/core/src/utils/Resources.java
+++ Code/core/src/utils/Resources.java
@@ -11,14 +11,14 @@ public class Resources {
     public static final String FONT_MENU_PATH = "fonts/orange juice 2.0.ttf";
     public static final String TEXT_BOX_PATH = "";
     public static final String MENU_BACKGROUND_PATH = "";
-    public static final String CHECK_PATH = "pawn.png";
+    public static final String CHECK_PATH = "Testing-Pawn.png";
 
     // RECURSOS PARTIDA
-    public static final String PAWN_PATH = "pawn.png";
-    public static final String KNIGHT_PATH = "Piece.png";
-    public static final String BISHOP_PATH = "";
-    public static final String ROOK_PATH = "";
-    public static final String QUEEN_PATH = "";
-    public static final String KING_PATH = "";
+    public static final String PAWN_PATH = "Testing-Pawn.png";
+    public static final String KNIGHT_PATH = "Testing-Knight.png";
+    public static final String BISHOP_PATH = "Testing-Bishop.png";
+    public static final String ROOK_PATH = "Testing-Rook.png";
+    public static final String QUEEN_PATH = "Testing-Queen.png";
+    public static final String KING_PATH = "Testing-King.png";
     
 }
