8b94c32990c4903b09db8599c9fd6f10978f54f9
jaiRodRod <jaezro@uma.es>
2023-03-22 18:12:10 +0100

    Pantalla de idiomas (Version Basica)
    Lo ideal seria poner algo para arrastrar y tener un menu al estilo minecraft para los idiomas

--- Code/core/src/game/chess/LanguageScreen.java
+++ Code/core/src/game/chess/LanguageScreen.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 
-import interaccionFichero.LectorLineas;
+import interaccionFichero.*;
 import utils.*;
 
 public class LanguageScreen extends AbstractScreen {
@@ -13,18 +13,32 @@ public class LanguageScreen extends AbstractScreen {
     TextButton home,exit,config;
     Text homeText,exitText,Language,configText;
     Image background,Logo,news;
-    LectorLineas languageReader, configReader;
+    LectorLineas languageReader, configReader, languageConfigReader;
+    EscritorLineas languageSettingWriter;
+    
+    Text spanishText,englishText;
+    TextButton spanish,english;
     
     @Override
     public void show() {
     	
     	//Abrir los ficheros de configuracion e idioma
     	configReader = new LectorLineas("files/config.txt"); //Lector del txt configuracion para sacar el idioma
+    	languageConfigReader = new LectorLineas("files/languages.txt"); //Lector del txt donde vienen todos los idiomas implementados
     	languageReader = new LectorLineas("files/lang/"+ configReader.leerLinea(1)); //Abrimos el idioma que toca del archivo configuracion
+    	languageSettingWriter = new EscritorLineas("files/config.txt"); //Para actualizar la configuracion abrimos el escritor en el config.txt 
     	
     	//Fuente Arial para probar
     	Language = new Text(Resources.FONT_MENU_PATH,100,Color.WHITE,5);
     	Language.setText(languageReader.leerLinea(6)); //Idioma = Linea 6
+    	
+    	//Menu idiomas
+    	spanishText = new Text(Resources.FONT_MENU_PATH,50,Color.WHITE,5);
+    	spanishText.setText(languageConfigReader.leerLinea(1)); //Español = Linea 1 
+    	englishText = new Text(Resources.FONT_MENU_PATH,50,Color.WHITE,5);
+    	englishText.setText(languageConfigReader.leerLinea(2)); //Ingles = Linea 2 
+    	
+    	//Menu inferior
     	homeText = new Text(Resources.FONT_MENU_PATH,28,Color.WHITE,5);
     	homeText.setText(languageReader.leerLinea(2)); //Inicio = Linea 2
     	configText = new Text(Resources.FONT_MENU_PATH,28,Color.WHITE,5);
@@ -32,12 +46,16 @@ public class LanguageScreen extends AbstractScreen {
     	exitText = new Text(Resources.FONT_MENU_PATH,28,Color.WHITE,5);
     	exitText.setText(languageReader.leerLinea(3)); //Salir = Linea 3
     	Language.setPosition(500,600);
+    	spanishText.setPosition(100, 400);
+    	englishText.setPosition(100, 300);
         homeText.setPosition(100,100);
         configText.setPosition(220,100);
         exitText.setPosition(480,100);
         home = new TextButton(homeText);
         config = new TextButton(configText);
         exit = new TextButton(exitText);
+        spanish = new TextButton(spanishText);
+        english = new TextButton(englishText);
         Logo = new Image("Logo.png");
         Logo.setPosition(800,-50);
         Logo.setSize(500, 500);
@@ -60,9 +78,22 @@ public class LanguageScreen extends AbstractScreen {
         Language.draw();
         Logo.draw(Render.Batch);
         //news.draw(Render.Batch);
+        spanish.establish(inputs, Render.Batch);
+        english.establish(inputs, Render.Batch);
+        
         home.establish(inputs, Render.Batch);
         config.establish(inputs, Render.Batch);
         exit.establish(inputs, Render.Batch);
+        
+        if(spanish.isSelected()) {
+        	languageSettingWriter.escribirLinea(1, "esp.txt"); //La linea 1 de la configuracion contiene el idioma
+        	Render.app.setScreen(new LanguageScreen());
+        }
+        if(english.isSelected()) {
+        	languageSettingWriter.escribirLinea(1, "eng.txt"); //El nombre es el nombre del archivo txt de idioma
+        	Render.app.setScreen(new LanguageScreen());
+        }
+        
         
         if(home.isSelected()){
             Render.app.setScreen(new MainScreen());
