c40c8048e71ba1bdf3bace733035739b6f76b785
JoseBueno30 <jbueno3830@uma.es>
2023-04-12 13:32:20 +0200

    draft beta 0.1

--- Code/core/src/game/chess/DraftScreen.java
+++ Code/core/src/game/chess/DraftScreen.java
@@ -16,7 +16,7 @@ import utils.*;
 public class DraftScreen extends AbstractScreen {
 	public static Stage stage;
 	
-	private Map<String, Piece> draft;
+	private Map<Image, Piece> draft;
 	
 	
 	Background background;
@@ -26,10 +26,9 @@ public class DraftScreen extends AbstractScreen {
 	public void show() {
 		stage = new Stage(new FitViewport(1280, 720));
 		stage.clear();
-		Gdx.input.setInputProcessor(Render.inputs);
+		Gdx.input.setInputProcessor(stage);
 		info=new PieceInfo(new Knight(true,3,3,new Board(70,615,-150)));
 		
-		initDraft();
 		
 		
 		background = new Background();
@@ -39,23 +38,32 @@ public class DraftScreen extends AbstractScreen {
 
 		stage.addActor(background);
 		stage.addActor(info);
+		initDraft();
 	}
 	
 	private void initDraft() {
-		draft = new HashMap<String, Piece>();
-		draft.put("Pawn", null);
-		draft.put("Knight", null);
-		draft.put("Rook", null);
-		draft.put("Bishop", null);
-		draft.put("Queen", null);
-		draft.put("King", null);
+		draft = new HashMap<Image, Piece>();
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		
+		int i = 0;
+		for(Image piece : draft.keySet()) {
+			piece.setSize(84, 84);
+			piece.setPosition(20 , 65 + (i*100));
+			i++;
+			stage.addActor(piece);
+		}
 	}
 	
 
 	@Override
 	public void render(float delta) {
 		Render.clearScreen();
-
+		
 		stage.draw();
 		stage.act();
 	}
