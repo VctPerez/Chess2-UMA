beebe651c36792b9dca3131b9cec98f97b39e81f
DavidBueno <davidbuenocarmona@uma.es>
2023-04-12 13:52:51 +0200

    Botones Añadidos en Draft

--- Code/core/src/game/chess/DraftScreen.java
+++ Code/core/src/game/chess/DraftScreen.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.utils.viewport.FitViewport;
@@ -21,6 +22,7 @@ public class DraftScreen extends AbstractScreen {
 	
 	Background background;
 	PieceInfo info;
+    TextButton end,next,back;
 
 	@Override
 	public void show() {
@@ -28,27 +30,27 @@ public class DraftScreen extends AbstractScreen {
 		stage.clear();
 		Gdx.input.setInputProcessor(stage);
 		info=new PieceInfo(new Knight(true,3,3,new Board(70,615,-150)));
-		
-		
-		
 		background = new Background();
 		background.setSize(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		
+		
 
 		// -------------------------------
 
 		stage.addActor(background);
-		stage.addActor(info);
+		stage.addActor(info);	
 		initDraft();
+		initButtons();
 	}
 	
 	private void initDraft() {
 		draft = new HashMap<Image, Piece>();
+		draft.put(new Image(Render.app.getManager().get(Resources.PAWN_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.KNIGHT_PATH, Texture.class)), null);
 		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
-		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
-		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
-		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
-		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
-		draft.put(new Image(Render.app.getManager().get(Resources.BISHOP_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.ROOK_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.QUEEN_PATH, Texture.class)), null);
+		draft.put(new Image(Render.app.getManager().get(Resources.KING_PATH, Texture.class)), null);
 		
 		int i = 0;
 		for(Image piece : draft.keySet()) {
@@ -59,13 +61,36 @@ public class DraftScreen extends AbstractScreen {
 		}
 	}
 	
+	private void initButtons() {
+		Text endText,nextText,backText;
+		endText = new Text(Resources.FONT_MENU_PATH,20,Color.WHITE,3);
+    	endText.setText("End"); 
+    	endText.setPosition(50, 50);
+    	nextText = new Text(Resources.FONT_MENU_PATH,20,Color.WHITE,3);
+    	nextText.setText("Next"); 
+    	nextText.setPosition(600, 50);
+    	backText = new Text(Resources.FONT_MENU_PATH,20,Color.WHITE,3);
+    	backText.setText("Back"); 
+    	backText.setPosition(300, 50);
+    	
+    	end = new TextButton(endText);
+        next = new TextButton(nextText);
+        back = new TextButton(backText);
+        
+        stage.addActor(end);
+		stage.addActor(next);
+		stage.addActor(back);
+	}
+	
 
 	@Override
 	public void render(float delta) {
 		Render.clearScreen();
-		
+		Render.Batch.begin();
 		stage.draw();
 		stage.act();
+		
+		Render.Batch.end();
 	}
 
 	@Override
