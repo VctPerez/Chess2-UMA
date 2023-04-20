202a03a631c730f580fdd34b3478a65f8c6ff883
GregorioMerchan <gregomerchan@uma.es>
2023-04-20 20:42:43 +0200

    Peon normal en Draft
    He metido el Peon normal en el draft para que pueda ser seleccionable

--- Code/core/src/game/chess/DraftScreen.java
+++ Code/core/src/game/chess/DraftScreen.java
@@ -97,6 +97,7 @@ public class DraftScreen extends AbstractScreen {
 		queens = new ArrayList<>();
 		kings = new ArrayList<>();
 		
+		pawns.add(Resources.PAWN_PATH);
 		pawns.add(Resources.WARDEN_PATH);
 		pawns.add(Resources.LANCER_PATH);
 		Collections.shuffle(pawns);
