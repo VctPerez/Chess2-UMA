4ad8d42a5adf97c92089648bdbcb926638c02d19
GregorioMerchan <gregomerchan@uma.es>
2023-04-20 19:27:35 +0200

    Midas y Black Paladin

--- Code/core/src/utils/Resources.java
+++ Code/core/src/utils/Resources.java
@@ -47,7 +47,7 @@ public class Resources {
     public static final String WARDEN_PATH = "sprites/Testing-Pieces/Pieces Modified/White_Warden.png";
     public static final String PALADIN_PATH = "sprites/Testing-Pieces/Pieces Modified/White_Paladin.png";
     public static final String RND_PATH = "sprites/Testing-Pieces/Pieces Modified/White_Joker.png";
-    public static final String MIDAS_PATH = "piece2.png";
+    public static final String MIDAS_PATH = "sprites/Testing-Pieces/Pieces Modified/White_Midas.png";
     
     public static final String RND_KNIGHT_PATH = "sprites/Testing-Pieces/Pieces Rnd Form/RndWhite_Knight.png";
     public static final String RND_ROOK_PATH = "sprites/Testing-Pieces/Pieces Rnd Form/RndWhite_Rook.png";
