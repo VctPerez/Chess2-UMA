4c1cef981003eed4b21145524a5ca22208a879ce
jaiRodRod <jaezro@uma.es>
2023-04-27 20:01:13 +0200

    pieceTestPart1

--- Code/core/src/tests/pieceTest.java
+++ Code/core/src/tests/pieceTest.java
@@ -5,10 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
-import elements.Board;
 import elements.Piece;
+
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -26,13 +28,28 @@ public class pieceTest {
 	public void terminate() {
 		pieza = null;
 	}
-   	
-	
-	//Pruebas
-	//color
 	
     @Test
-    public void crearPieza(){
-    	
+    public void colorTest(){
+    	assertEquals(pieza.color(),null,"Si no se construye una pieza que derive de Piece es un valor nulo");
+    }
+    
+    @Test
+    public void getPosTest()
+    {
+    	Vector2 vector = mock(Vector2.class);
+    	vector.x = 0;
+    	vector.y = 0;
+    	assertEquals(pieza.getPos().x,vector.x,"El valor de x es el adecuado por defecto");
+    	assertEquals(pieza.getPos().y,vector.y,"El valor de y es el adecuado por defecto");
+    }
+    
+    @Test
+    public void setAliveTest()
+    {
+    	pieza.setAlive(true);
+    	assertEquals(pieza.alive,true,"En un principio debe estar viva");
+    	pieza.setAlive(false);
+    	assertEquals(pieza.alive,false,"Despues se pone la pieza en dead");
     }
 }
