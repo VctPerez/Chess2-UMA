85c175947f865f72425c413219b1033c385f7203
jaiRodRod <jaezro@uma.es>
2023-04-27 21:37:57 +0200

    pieceTestv1

--- Code/core/src/tests/pieceTest.java
+++ Code/core/src/tests/pieceTest.java
@@ -5,12 +5,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
+import elements.Board;
 import elements.Piece;
-
+import elements.Tile;
+import elements.pieces.Rook;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -31,25 +32,43 @@ public class pieceTest {
 	
     @Test
     public void colorTest(){
-    	assertEquals(pieza.color(),null,"Si no se construye una pieza que derive de Piece es un valor nulo");
+    	Tile tileMock = mock(Tile.class);
+    	Board boardMock = mock(Board.class);
+    	when(boardMock.getTile(0, 0)).thenReturn(tileMock);
+    	when(tileMock.getX()).thenReturn(0f);
+    	when(tileMock.getY()).thenReturn(0f);
+    	
+    	pieza = new Rook(true,0,0,boardMock, mock(Texture.class));
+    	
+    	assertEquals(pieza.color(),true,"Si se construye una pieza que deriva de Piece se describe el atributo color correctamente");
+    }
+    
+    @Test
+    public void aliveTest()
+    {
+    	Tile tileMock = mock(Tile.class);
+    	Board boardMock = mock(Board.class);
+    	when(boardMock.getTile(0, 0)).thenReturn(tileMock);
+    	when(tileMock.getX()).thenReturn(0f);
+    	when(tileMock.getY()).thenReturn(0f);
+    	
+    	pieza = new Rook(true,0,0,boardMock, mock(Texture.class));
+    	
+    	assertEquals(pieza.alive,true,"En un principio debe estar viva");
     }
     
     @Test
     public void getPosTest()
     {
-    	Vector2 vector = mock(Vector2.class);
-    	vector.x = 0;
-    	vector.y = 0;
-    	assertEquals(pieza.getPos().x,vector.x,"El valor de x es el adecuado por defecto");
-    	assertEquals(pieza.getPos().y,vector.y,"El valor de y es el adecuado por defecto");
-    }
-    
-    @Test
-    public void setAliveTest()
-    {
-    	pieza.setAlive(true);
-    	assertEquals(pieza.alive,true,"En un principio debe estar viva");
-    	pieza.setAlive(false);
-    	assertEquals(pieza.alive,false,"Despues se pone la pieza en dead");
+    	Tile tileMock = mock(Tile.class);
+    	Board boardMock = mock(Board.class);
+    	when(boardMock.getTile(1, 1)).thenReturn(tileMock);
+    	when(tileMock.getX()).thenReturn(0f);
+    	when(tileMock.getY()).thenReturn(0f);
+    	
+    	pieza = new Rook(true,1,1,boardMock, mock(Texture.class));
+    	
+    	assertEquals(pieza.getPos().x,1,"El valor de x es el adecuado al del constructor");
+    	assertEquals(pieza.getPos().y,1,"El valor de y es el adecuado al del constructor");
     }
 }
