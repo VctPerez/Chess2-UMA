da9e8dd63387d01a5817bb5ef41ddc51370f5380
AdrianTorremochaUMA <adriantorremocha@uma.es>
2023-04-30 19:57:03 +0200

    Animaciones Menu y cambios generales
    El cambio más importante es que he movido todos los aspectos genéricos de las clases del menú a una nueva clase abstracta, dejando el código de cada pantalla muy compacto y facil de leer.
    
    Tambien he añadido animaciones de transicion en el cambio de pantallas.
    
    Por último, he cambiado la traduccion de configuracion para que sea "Ajustes" y "Settings" en la MAINSCREEN

---
+++ Code/core/src/game/chess/AbstractMenuScreen.java
@@ -0,0 +1,162 @@
+package game.chess;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+
+import interaccionFichero.LectorLineas;
+import utils.Render;
+import utils.TextButton;
+
+public abstract class AbstractMenuScreen extends AbstractScreen{
+
+	protected Stage stage;
+	protected Table table;
+	
+	protected TextButton[] textButton;
+    protected Label title;
+    
+    protected LectorLineas configReader;
+    
+    protected int activatedTextButton;
+    protected boolean animationStarted;
+	
+	@Override
+	public void show() {
+    	stage = new Stage(new FitViewport(1280, 720));
+    	table = new Table();
+    	
+//    	table.debug();
+    	
+    	createTableElements();
+        setupTable();
+        addActors();
+        
+    	//Inputs de los botones
+        for(int i = 0 ; i < textButton.length ; i++) {
+        	final int index = i;
+        	textButton[i].addListener(new ClickListener() {
+        		@Override
+        		public void clicked(InputEvent event, float x, float y) {
+        			super.clicked(event, x, y);
+        			activatedTextButton = index;
+        			addExitAnimation(800f,0.25f,0.5f);
+        			animationStarted = true;
+        		}
+        	});
+        }
+        
+        animationStarted = false;
+        
+        addEnterAnimation();
+        
+        Gdx.input.setInputProcessor(stage);
+	}
+
+	@Override
+	public void render(float delta) {
+        Render.clearScreen();
+
+        Render.camera.update();
+        Render.Batch.setProjectionMatrix(Render.camera.combined);
+        
+        //---------------
+        
+        stage.act();
+        stage.draw();
+        
+        if(animationStarted && checkAnimationsFinished()) {
+        	selectScreen(activatedTextButton);
+        }
+        
+        //---------------
+        
+	}
+	
+    protected abstract void createTableElements();
+    
+    protected void setupTable() {
+    	table.setFillParent(true);
+    	table.left().pad(50);
+    	table.defaults().left().space(40);
+    	table.add(title);
+    	table.row();
+    	for (int i = 0 ; i < textButton.length ; i++) {
+    		table.add(textButton[i]);
+        	table.row();
+    	}
+    }
+    
+    protected void addActors() {
+    	stage.addActor(Render.menuBG);
+    	stage.addActor(table);
+    }
+	
+    /**
+     * Metodo que añade la animacion de salida a los elementos de la UI.
+     * Cuando se pulsa un boton, van saliendo de la pantalla con un delay.
+     * Una vez acaba la animación se realiza la accion del botón gracias a {@link selectScreen}.
+     * @param distance Distancia en píxeles a desplazar
+     * @param delay Retraso entre animaciones
+     * @param time Tiempo que tarda en realizarse la accion
+     */
+    protected void addExitAnimation(float distance, float delay, float time) {
+    	
+    	title.addAction(Actions.fadeOut(0.25f));
+    	
+    	for(int i = 0; i < textButton.length ; i++) {
+			textButton[i].addAction(Actions.delay(delay * i, Actions.moveBy(-distance, 0, time)));
+			textButton[i].setDisabled(true);
+    	}
+    }
+    
+    /**
+     * Método que añade la animación de entrada.
+     * Se establece el alpha a 0 y luego se incrementa con un fadeIn
+     */
+    protected void addEnterAnimation() {
+    	table.addAction(Actions.sequence(Actions.alpha(0), Actions.fadeIn(0.5f)));
+    }
+    
+    /**
+     * Dado el indice de un TextButton, realiza su accion correspondiente
+     * @param button
+     */
+    protected abstract void selectScreen(int button);
+    
+    /**
+     * Devuelve TRUE si las animaciones de salida han terminado. En caso contrario, FALSE
+     */
+    protected boolean checkAnimationsFinished() {
+    	boolean finished = true;
+    	int i = 0;
+    	
+    	while(i < textButton.length && finished) {
+    		finished = !textButton[i].hasActions();
+    		i++;
+    	}
+    	
+    	return finished;
+    }
+
+	@Override
+	public void resize(int width, int height) {
+        stage.getViewport().update(width, height);
+        
+        Render.SCREEN_WIDTH = stage.getViewport().getScreenWidth();
+        Render.SCREEN_HEIGHT = stage.getViewport().getScreenHeight();
+
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	    stage.dispose();
+	}
+	
+}
--- Code/core/src/game/chess/ModeScreen.java
+++ Code/core/src/game/chess/ModeScreen.java
@@ -1,71 +1,42 @@
 package game.chess;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
 import interaccionFichero.LectorLineas;
 import utils.Render;
-import utils.Resources;
 import utils.Settings;
 import utils.TextButton;
 
-public class ModeScreen extends AbstractScreen{
-	public static Stage stage;
-	private Table table;
-
-    private TextButton[] textButton;
-    private Label title;
+public class ModeScreen extends AbstractMenuScreen{
     private LectorLineas languageReader, configReader;
     
     @Override
     public void show() {
-    	stage = new Stage(new FitViewport(1280, 720));
-    	table = new Table();
-    	table.setFillParent(true);
-//    	table.debug();
-    	
     	//Abrir los ficheros de configuracion e idioma
     	configReader = new LectorLineas("files/config.txt"); //Lector del txt configuracion para sacar el idioma
     	languageReader = new LectorLineas("files/lang/"+ configReader.leerLinea(Settings.language) + "matchmaking.txt"); //Abrimos el idioma que toca del archivo configuracion
     	
-    	createTableElements();
-        setupTable();
-        addActors();
-        
-        Gdx.input.setInputProcessor(stage);
-    }
-
-    @Override
-    public void render(float delta) {
-        Render.clearScreen();
-
-        Render.camera.update();
-        Render.Batch.setProjectionMatrix(Render.camera.combined);
-
-        //---------------
-        
-        stage.act();
-        stage.draw();
-        
-        if(textButton[0].isPressed()) {
-        //	Render.app.setScreen(Render.CREATEMATCHSCREEN);
-        	Render.DraftController=1;
-        	Render.app.setScreen(Render.DRAFTSCREEN);
-        }else if(textButton[1].isPressed()) {
-        	Render.app.setScreen(new GameScreen());
-        }else if(textButton[2].isPressed()) {
-        	Render.app.setScreen(Render.MATCHMAKINGSCREEN);
+    	super.show();
+    	
+    	//Inputs de los botones
+        for(int i = 0 ; i < textButton.length ; i++) {
+        	textButton[i].addListener(new ClickListener() {
+        		@Override
+        		public void clicked(InputEvent event, float x, float y) {
+        			super.clicked(event, x, y);	
+        			if(activatedTextButton == 0) {
+        				Render.DraftController = 1;
+        			}
+        		}
+        	});
         }
         
-        //-----------------
-
     }
     
-    private void createTableElements() {
+    @Override
+    protected void createTableElements() {
     	
     	textButton = new TextButton[3];
     	
@@ -74,36 +45,22 @@ public class ModeScreen extends AbstractScreen{
     	textButton[0] = new TextButton(languageReader.leerLinea(6));//Modified
     	textButton[1] = new TextButton(languageReader.leerLinea(7));//Classic
     	textButton[2] = new TextButton(languageReader.leerLinea(4));//Return
-    }
-    
-    private void addActors() {
-    	stage.addActor(Render.menuBG);
-    	stage.addActor(table);
-    }
-    
-    private void setupTable() {
-    	table.left().pad(50);
-    	table.defaults().left().space(40);
-    	table.add(title);
-    	table.row();
-    	for (int i = 0 ; i < textButton.length ; i++) {
-    		table.add(textButton[i]);
-        	table.row();
+    	
+    	for(int i = 0; i < textButton.length; i++) {
+    		textButton[i].addAnimation();
     	}
     }
     
-    @Override
-    public void resize(int width, int height) {
-        Render.SCREEN_WIDTH = width;
-        Render.SCREEN_HEIGHT = height;
-        Render.camera.setToOrtho(false, width, height);
-        
-        stage.getViewport().update(width, height);
-        
-    }
-
-    @Override
-    public void dispose() {
-        stage.dispose();
-    }
+	@Override
+	protected void selectScreen(int button) {
+    	if(button == 0) {
+    		Render.app.setScreen(Render.DRAFTSCREEN);
+    	}else if(button == 1) {
+    		Render.app.setScreen(new GameScreen());
+    	}else if(button == 2) {
+    		Render.app.setScreen(Render.MATCHMAKINGSCREEN);
+    	}
+	}
+    
+    
 }
